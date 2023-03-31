465c9c05a22cbcb2d328de2d311e5b5344d31dd2
Jirosworld <jolarti@gmail.com>
2023-03-31 20:18:20 +0200

    :sparkles: add dynamic templates for GET users and GET leads

--- README.md
+++ README.md
@@ -6,23 +6,68 @@ Start by cloning this repo
 
 git clone https://github.com/skolo-online/crm-course.git
 
-pip install flask
+### Start
 
-Then install all the requirements
+Install all the requirements
 
-## youtube tutorials
+`$ virtualenv appenv`
+
+    $ source appenv/bin/activate
+
+`$ pip install flask`
+
+`$ pip install requests`
+
+`$ pip freeze`
+
+### Create local config
+
+To keep your API-key secret:
+
+Add inside your file config.py, in the first class Config Object:
+`AIRTABLE_KEY = 'keyiiiiaaaa'`
+
+See your users table:
+http://192.168.178.59:8888/users
+
+## Youtube tutorials
 
 part 1: https://www.youtube.com/watch?v=1hjC1J5FfQ0
 
 part 2: https://www.youtube.com/watch?v=WKpWtZ7V79w
 
-## get Airtable API
+first create empty databases on Airtable
+tool: https://www.uuidgenerator.net/
 
+## Get Airtable API
+
+CreateReadUpdateDelete
 https://airtable.com/pricing
+
+Airtable generated documntation for these tables:
+https://airtable.com/app4vQ4CtiTItA8Rn/api/docs#curl/metadata
 
 ## About this app
 
-It's purer Python with Flas, without any Django
+It's pure Python with Flask, without any Django
 
 - Running on http://127.0.0.1:8888
 - Running on http://192.168.178.59:8888
+
+## Explanation about the data
+
+The date is returned as a JSON object (becasue the GET method comes as a text object from the json.loads request method) with an array inside:
+
+    {
+    "records": [
+        {
+            "id": "recQBDSc7p2MuRhpJ",
+            "createdTime": "2023-03-31T13:07:58.000Z",
+            "fields": {
+                "FirstName": "Big",
+                "Priority": "1",
+                "JobDescription": "CEO",
+
+You can GET individual fields from the Records array like such:
+
+`result['records'][0]['fields']['FirstName']`
--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template
 import config
+import requests
+import json
+
 
 def page_not_found(e):
   return render_template('404.html'), 404
@@ -10,11 +13,80 @@ app.config.from_object(config.config['development'])
 
 app.register_error_handler(404, page_not_found)
 
+xheaders = { 'authorization': 'Bearer {}'.format(app.config['AIRTABLE_KEY']), 'content-type': 'application/json' }
+
 
 @app.route('/')
 def index():
     return render_template('index.html', **locals())
 
+@app.route('/users')
+def users():
+    url = 'https://api.airtable.com/v0/app4vQ4CtiTItA8Rn/Teams?maxRecords=10'
+    r = requests.get(url, headers=xheaders)
+    result = json.loads(r.text)
+    # print(result['records'])
+    return render_template('users.html', **locals())
+
+@app.route('/leads')
+def leads():
+    url = 'https://api.airtable.com/v0/app4vQ4CtiTItA8Rn/Lead?maxRecords=5'
+    r = requests.get(url, headers=xheaders)
+    result = json.loads(r.text)
+    # print(result['records'])
+    return render_template('leads.html', **locals())
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='8888', debug=True)
+
+# 
+# extra, unused templates for now
+@app.route('/blank')
+def blank():
+    return render_template('blank.html', **locals())
+    
+@app.route('/buttons')
+def buttons():
+    return render_template('buttons.html', **locals())
+
+@app.route('/cards')
+def cards():
+    return render_template('cards.html', **locals())
+
+@app.route('/charts')
+def charts():
+    return render_template('charts.html', **locals())
+    
+@app.route('/forgot-password')
+def forgot_password():
+    return render_template('forgot-password.html', **locals())
+
+@app.route('/login')
+def login_page():
+    return render_template('login.html', **locals())
+
+@app.route('/register')
+def register_page():
+    return render_template('register.html', **locals())
+    
+@app.route('/tables')
+def tables():
+    return render_template('tables.html', **locals())
+
+@app.route('/utilities-animation')
+def utilities_animation():
+    return render_template('utilities-animation.html', **locals())
+
+@app.route('/utilities-border')
+def utilities_border():
+    return render_template('utilities-borderr.html', **locals())
+
+@app.route('/utilities-color')
+def utilities_color():
+    return render_template('utilities-color.html', **locals())
+
+@app.route('/utilities-other')
+def utilities_other():
+    return render_template('utilities-other.html', **locals())
+
